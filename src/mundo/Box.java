aa338e2832ef31fd50b2d5955dfe3ca762e0df6c
EmmanuelZuluaga <42072847+EmmanuelZuluaga@users.noreply.github.com>
2018-08-19 17:24:25 -0500

    New class box
    It have been adding in the model package the new class "box"

---
+++ src/mundo/Box.java
@@ -0,0 +1,19 @@
+package mundo;
+
+public class Box {
+	
+	private float number;
+	
+	public Box(float number) {
+		// TODO Auto-generated constructor stub
+		this.number=number;
+	}
+	public float getNumber() {
+		return number;
+	}
+	public void setNumber(float number) {
+		this.number=number;
+	}
+	
+	
+}
